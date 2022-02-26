9d14e245a8a77dc260ffbf909f342bb9ee1908d9
Hoang Nhat <hoangnhat2772001@gmail.com>
2022-02-26 22:10:38 +0700

    end the band

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 /**Buy ticket js */
-const buyBtns = document.querySelectorAll('.js-buy-ticket')
+const buyBtns = document.querySelectorAll(".js-buy-ticket");
 const modal = document.querySelector(".js-modal");
 const removeModal = document.querySelector(".js-modal-close");
 const modalContainer = document.querySelector(".js-modal-container");
@@ -13,31 +13,36 @@ function removeBuyTicket() {
 }
 
 for (const buyBtn of buyBtns) {
-    buyBtn.addEventListener('click', showBuyTicket)
+    buyBtn.addEventListener("click", showBuyTicket);
 }
-removeModal.addEventListener('click', removeBuyTicket)
-modalContainer.addEventListener('click', function(event) {
-    event.stopPropagation()
-})
+removeModal.addEventListener("click", removeBuyTicket);
+modalContainer.addEventListener("click", function (event) {
+    event.stopPropagation();
+});
 
 /*Mobile menu button Open/Close*/
-let header = document.getElementById('header');
-let mobileMenuBtn = document.querySelector('.mobile-menu');
+let header = document.getElementById("header");
+let mobileMenuBtn = document.querySelector(".mobile-menu");
 let headerHeight = header.clientHeight;
 console.log(mobileMenuBtn);
-mobileMenuBtn.onclick = function() {
-        let isClosed = header.clientHeight === headerHeight;
-        if (isClosed) {
-            header.style.height = 'auto';
-        } else {
-            header.style.height = null;
-        }
+mobileMenuBtn.onclick = function () {
+    let isClosed = header.clientHeight === headerHeight;
+    if (isClosed) {
+        header.style.height = "auto";
+    } else {
+        header.style.height = null;
     }
-    /*Auto close when choose menu */
+};
+/*Auto close when choose menu */
 let menuItems = document.querySelectorAll("#nav li a[href*='#']");
 for (let i = 0; i < menuItems.length; i++) {
     let menuItem = menuItems[i];
-    menuItem.onclick = function() {
-        header.style.height = null;
-    }
+    menuItem.onclick = function (event) {
+        let isParentMenu = menuItem.nextElementSibling;
+        if (isParentMenu) {
+            event.preventDefault();
+        } else {
+            header.style.height = null;
+        }
+    };
 }
